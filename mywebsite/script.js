5b78fa27322071ec9a389d3a8638a1f1402015be
AudieRaymos <audriannakraymos@gmail.com>
2021-11-03 16:15:42 -0700

    all theme if statement set

--- mywebsite/script.js
+++ mywebsite/script.js
@@ -13,4 +13,16 @@ function setTheme(mode){
 if(mode == 'light'){
     document.getElementById('theme-style').href = 'default.css'
 }
+
+if(mode == 'blue'){
+    document.getElementById('theme-style').href = 'blue.css'
+}
+
+if(mode == 'green'){
+    document.getElementById('theme-style').href = 'green.css'
+}
+
+if(mode == 'purple'){
+    document.getElementById('theme-style').href = 'purple.css'
+}
 }
